0449163db28a331d6ee0067452356b43d131ff43
CristianN01 <cristiannechifor71@gmail.com>
2024-05-21 18:25:17 +0200

    aggiunto timer di 30 secondi, dopodiche i numeri in pagina scompariranno

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 const pageNumbers =[];
+let userSeconds = 30;
 
 for (let index = 0; index < 5 ; index++) {
     let casualNumbers = randomNumbers()
@@ -6,20 +7,21 @@ for (let index = 0; index < 5 ; index++) {
 }
 console.log(pageNumbers)
 
+// ? Selezionato la section nel file html, creato un elemento div al suo interno aggiungendolo con append, e poi aggiunti tutti i p all'interno del div con il ciclo for.
 const section = document.querySelector('section.numbers')
 const list = document.createElement('div')
 section.appendChild(list)
 list.classList.add('color')
 
+/** Funzione per crreare un p per ogni index presente nell'array
+ * 
+ */
 for (let index = 0; index < pageNumbers.length; index++) {
     const element = document.createElement('p')
     element.append(pageNumbers[index])
     list.appendChild(element)
 }
-// element.append(pageNumbers)
 
-
-// pageNumbers.innerHTML =
 
 /** Funzione che genera un numero randomico tra 1 e 100
  * 
@@ -29,3 +31,14 @@ function randomNumbers(){
     let random = Math.floor(Math.random() * 101)
     return random;
 }
+
+
+
+const timer = setInterval( function(){
+    userSeconds--; // % sottrai un secondo al timer 
+    console.log(userSeconds);
+    if( userSeconds <= 0){ // > se il timer ha raggiunto un numero di secondi uguale o minore a 0
+        document.querySelector('section.numbers').innerHTML = "";
+        clearInterval(timer)
+    }
+}, 1000);
